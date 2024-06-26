746b0cb7e8bafcfaebd0f4953c61c2aa3aba8761
AndrejBardakoski <86916133+AndrejBardakoski@users.noreply.github.com>
2024-06-26 18:50:10 +0200

    remove IDE and build files

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_mk_rkeview_HiltApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "mk.rkeview.HiltApp"
-)
-public final class _mk_rkeview_HiltApp {
-}
--- app/build/tmp/kapt3/stubs/debug/mk/rkeview/theme/ColorKt.java
+++
@@ -1,9 +0,0 @@
-package mk.rkeview.theme;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\n\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082\u0004\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0001X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0003"}, d2 = {"DarkColors", "Landroidx/compose/material/Colors;", "LightColors", "app_debug"})
-public final class ColorKt {
-    @org.jetbrains.annotations.NotNull
-    private static final androidx.compose.material.Colors LightColors = null;
-    @org.jetbrains.annotations.NotNull
-    private static final androidx.compose.material.Colors DarkColors = null;
-}
