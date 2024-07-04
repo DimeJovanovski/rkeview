4919198bb7bdf2ea2e814810d76678865ad3c6cf
Dimitar Jovanovski <jovanovski.dimitar@outlook.com>
2024-07-04 21:01:24 +0200

    Added settings button to navigate to SettingsScreen

--- app/src/main/java/mk/rkeview/ui/screens/SettingsScreen.kt
+++ app/src/main/java/mk/rkeview/ui/screens/SettingsScreen.kt
@@ -1,14 +1,20 @@
 package mk.rkeview.ui.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import mk.rkeview.ui.components.ScreenTemplate
 
 /**
- * TODO: Settings screen for app configuration by user.
+ * Settings screen for user configurations.
  *
  * @param navController - for app navigation
  */
@@ -17,7 +23,21 @@ import mk.rkeview.ui.components.ScreenTemplate
 fun SettingsScreen(navController: NavController) {
   ScreenTemplate(
     navController = navController,
-    content = { }
+    content = { SettingsScreenContent() }
+  )
+}
+
+@Composable
+fun SettingsScreenContent() {
+  /**
+   * TODO: Implement the screen and connect it with Firebase so the user can set
+   * notifications for price changes of different resources.
+   */
+  Text(
+    text = "Settings",
+    fontSize = 32.sp,
+    fontWeight = FontWeight.Bold,
+    modifier = Modifier.padding(vertical = 16.dp)
   )
 }
 
