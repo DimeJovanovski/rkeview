3bbf4520fd28d9a76e7038fb61ab339d0a5fef38
Dimitar Jovanovski <jovanovski.dimitar@outlook.com>
2024-07-28 15:06:44 +0200

    Created UI for Settings screen

--- app/src/main/java/mk/rkeview/ui/screens/SettingsScreen.kt
+++ app/src/main/java/mk/rkeview/ui/screens/SettingsScreen.kt
@@ -1,7 +1,14 @@
 package mk.rkeview.ui.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -11,8 +18,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import mk.rkeview.resourceComponents.ResourceType
 import mk.rkeview.theme.RKEviewTheme
 import mk.rkeview.theme.ThemeViewModel
+import mk.rkeview.ui.components.ResourceTypeNotificationToggleField
 import mk.rkeview.ui.components.ScreenTemplate
 
 /**
@@ -37,12 +46,35 @@ fun SettingsScreenContent() {
    * TODO: Implement the screen and connect it with Firebase so the user can set
    * notifications for price changes of different resources.
    */
-  Text(
-    text = "Settings",
-    fontSize = 32.sp,
-    fontWeight = FontWeight.Bold,
-    modifier = Modifier.padding(vertical = 16.dp)
-  )
+  Box(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(horizontal = 16.dp),
+  ) {
+
+    LazyColumn(
+      modifier = Modifier
+        .fillMaxSize(),
+      verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+      item {
+        Text(
+          text = "Подеси нотификации",
+          fontSize = 30.sp,
+          fontWeight = FontWeight.Bold,
+          modifier = Modifier.padding(vertical = 16.dp)
+        )
+      }
+
+      items(ResourceType.entries) { resourceType ->
+        Row(
+          modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+          ResourceTypeNotificationToggleField(resourceType)
+        }
+      }
+    }
+  }
 }
 
 @Preview
